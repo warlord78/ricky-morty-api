7cd0acd74ed81942c41e2636eb5f2f02625baaee
Juan Gui Arenas <juan.guihd@gmail.com>
2022-02-25 21:49:47 -0500

    primera version 01 del proyecto rick y morty

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import imageRickMorty from './img/rickymorty1.png';
 import './App.css';
+import { useState } from 'react';
+import Characters from './Components/Characters';
 
 function App() {
+  
+  const [characters, setCharacters] = useState(null);
+  const reqApi = async() => {
+  const api = await fetch('https://rickandmortyapi.com/api/character');
+  const characterApi = await api.json();
+  setCharacters(characterApi.results); 
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1 className="title">Rick y Morty</h1>
+      {characters ?(
+        <Characters characters={characters} setCharacters={setCharacters}/>
+      ):(
+      <>
+      <img src={imageRickMorty} alt="Rick y Morty" className='img-home' />
+      <button onClick={reqApi} className="btn-search">
+        Buscar Personajes
+      </button>
+      </>
+      )}     
       </header>
     </div>
   );
---
+++ src/Components/Characters.js
@@ -0,0 +1,57 @@
+export default function Characters(props) {
+
+const { characters, setCharacters} = props;
+
+const resetCharacter = () => {
+    
+    setCharacters(null);
+}
+
+console.log(characters); 
+return (
+    <div className="characters">
+       <h1>Personajes</h1>
+       <span className="back-home" onClick={resetCharacter}>Volver a la home</span>
+       <div className="container-characters">
+           {characters.map((character, index) => (
+              
+            <div className="character-container" key={index}>
+              <div>
+                 <img src={character.image} alt={character.name} />
+              </div>
+              <div> 
+                  <h3>{character.name}</h3>
+                  <h6>
+                      {character.status === "Alive" ? (
+                        <>
+                        <span className="alive" />
+                        Alive
+                        </>
+                      ):(
+                        <>
+                        <span  className="dead"/>
+                        Dead
+                        </>
+                      )}
+                  </h6>
+                  <p>
+                    <span className="text-grey">Episodios:</span>
+                    <span>{character.episode.length}</span>
+                  </p>
+                  <p>
+                      <span className="text-grey">Especie:</span>
+                      <span>{character.species}</span>
+                  </p>
+                  <p>
+                      <span className="text-grey">Locacion:</span>
+                      <span>{character.location.name}</span>
+                  </p>
+              </div>
+            </div>
+            ))}
+       </div> 
+       <span className="back-home" onClick={resetCharacter }>Volver a la home</span>
+    </div>
+    
+  );
+}
